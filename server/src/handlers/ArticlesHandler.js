bfe6e17166725265492dcf4291a3ed0aa3eaa3e6
Desire <dafewou@gmail.com>
2019-01-12 13:05:01 +0800

    ArticlesHandler to fetch articles from any 10 sources

---
+++ server/src/handlers/ArticlesHandler.js
@@ -0,0 +1,34 @@
+import Source from "../model/Source"
+import { ResourceNotFoundError } from "../errors"
+import Article from "../model/Article"
+
+const onGet = async (req, res, next) => {
+  const { category, language } = req.query
+
+  try {
+    // fetch sources information
+    const sources = await Source.fetchAll(category, language)
+    if (sources.length === 0) {
+      next(new ResourceNotFoundError("Not information source available"))
+      return
+    }
+
+    // Reduce sources to one string text for fetching articles
+    const articleSources = sources.reduce((acc, source) => {
+      return `${acc},${source.id}`
+    }, "")
+
+    // Fetch all articles
+    const articles = await Article.fetchAll(articleSources, category, language)
+
+    // Response
+    return res.json({
+      articles
+    })
+  } catch (err) {
+    next(err)
+  }
+}
+
+const ArticlesHandler = { onGet }
+export default ArticlesHandler

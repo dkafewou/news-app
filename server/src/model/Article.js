2012514dbe1c06634e9fcd5eb2c4ea95baa89812
Desire <dafewou@gmail.com>
2019-01-12 14:22:59 +0800

    Update article model

--- server/src/model/Article.js
+++ server/src/model/Article.js
@@ -1,11 +1,8 @@
 import API from "../api"
 import { DatabaseError } from "../errors"
 
-const DEFAULT_CATEGORY = "technology"
-const DEFAULT_LANGUAGE = "en"
-
 export default class Article {
-  constructor({ rows }) {
+  constructor(rows) {
     Object.assign(this, rows)
   }
 
@@ -13,17 +10,13 @@ export default class Article {
    * Fetch all articles
    * @static
    * @param {string} sources - The sources to fetch articles from
-   * @param {string} category - The category source information to fetch
-   * @param {string} language - The language source information to fetch
    * @param newsAPI - Third party library instance use to fetch news
    * @returns {Promise<[Article]>}
    */
-  static async fetchAll(sources, category = DEFAULT_CATEGORY, language = DEFAULT_LANGUAGE, newsAPI = API.NewsAPI.shared) {
+  static async fetchAll(sources, newsAPI = API.NewsAPI.shared) {
     try {
       const response = await newsAPI.v2.topHeadlines({
-        sources:  sources,
-        category: category,
-        language: language
+        sources: sources
       })
 
       return response.articles.map(article => new Article(article))

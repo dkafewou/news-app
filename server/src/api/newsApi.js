55499d314fdf421249b70a4de22605245b57f8be
Desire <dafewou@gmail.com>
2019-01-12 14:22:05 +0800

    Use a valid API_KEY

--- server/src/api/newsApi.js
+++ server/src/api/newsApi.js
@@ -1,7 +1,7 @@
 import NewsApi from "newsapi"
 import Config from "../helpers/Config"
 
-const API_KEY = Config.shared.requireProduction("API_KEY", "4463838f6daf4f53a36605feb6b1a7b9")
+const API_KEY = Config.shared.requireProduction("API_KEY", "6bf1ed229e5c43ae8cf2f53d57da17ba")
 
 let instance
 

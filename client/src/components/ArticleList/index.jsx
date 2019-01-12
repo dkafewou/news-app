6e415336406b747feb3827096a1bb7c3c4dd5cdd
Desire <dafewou@gmail.com>
2019-01-12 23:14:51 +0800

    Add socket logic to update feed

--- client/src/components/ArticleList/index.jsx
+++ client/src/components/ArticleList/index.jsx
@@ -1,8 +1,12 @@
 import React from "react"
 import socketIOClient from "socket.io-client"
 import styles from "./ArticlesList.less"
+import { Alert } from "reactstrap"
 import BasePage from "../BasePage"
 import ArticleListItem from "../ArticleListItem"
+import { urlForEndpoint } from "../../api/helperFunctions"
+
+const endpoint = urlForEndpoint("")
 
 export default class ArticleList extends React.Component {
   constructor(props) {
@@ -12,10 +16,22 @@ export default class ArticleList extends React.Component {
   componentDidMount() {
     const { actions } = this.props
     actions.fetchArticles()
+
+    const socket = socketIOClient(endpoint)
+    socket.on("NewsFeed", articles => actions.updateNewsFeed(articles))
   }
 
   render() {
     const { isFetching, articles, error } = this.props
+    if (error) {
+      return (
+        <BasePage isLoading={isFetching}>
+          <div className="container">
+            <Alert color="danger">Something went wrong: {error.message}</Alert>
+          </div>
+        </BasePage>
+      )
+    }
     const articlesNodes = articles.map((article, index) => (<ArticleListItem id={index} article={article}/>))
 
     return (
